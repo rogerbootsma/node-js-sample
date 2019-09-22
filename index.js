90fb0062e7f785990a80ebed0091edc4db5a6b80
roger bootsma <rogerbootsma@gmail.com>
2019-09-22 16:19:33 +0200

    Update index.js
    change 1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('spacestation-x.com is now live, hopefully')
 })
 
 app.listen(app.get('port'), function() {
